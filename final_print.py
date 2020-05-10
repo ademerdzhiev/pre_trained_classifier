46e1030c01072df40e901cacb3bedf7ab0ba6f6e
Angel Petrov <a.demerdzhiev@gmail.com>
2020-05-10 23:46:55 +0300

    pre-trained classifier initial commit

---
+++ final_print.py
@@ -0,0 +1,3 @@
+import sys_res
+print_tuple = ("vgg_pet-images.txt", "resnet_pet-images.txt", "alexnet_pet-images.txt")
+sys_res.sys_res(print_tuple)
---
+++ sys_res.py
@@ -0,0 +1,66 @@
+# PROGRAMMER: Angel
+# DATE CREATED: 24.04.2020
+# REVISED DATE: 10.05.2020
+
+
+def sys_res(tuple_files_res):
+    """
+    Prints the final results frоm the "Use a Pre-trained Image Classifier to Identify Dog Breeds"
+    systematized in a table
+       Parameters:
+         tuple_files_dic - Tuple with the names of the .txt output files from the run_models_batch.sh script
+       Returns:
+              None - prints the results as above mentioned.
+       """
+
+    # dictionary with key, the name of the output file for the respective cnn, and the value is a list with following results:
+    # % dogs-correct, % match-labels, % breeds-correct, % not-a-dog-correct
+    final_res = dict()
+
+    # loop to read through each files and assign the correct values in the final_res dictionary
+    for file in tuple_files_res:
+        res = []
+        with open(file, 'r') as res_file:
+            line = res_file.readline()
+            while line != "":
+                line = line.strip("\n")
+                if "pct_correct_dogs" in line:
+                    line = line.split(": ")
+                    res.append(line[1].strip(" "))
+
+                if "pct_match" in line:
+                    line = line.split(": ")
+                    res.append(line[1].strip(" "))
+
+                if "pct_correct_breed" in line:
+                    line = line.split(": ")
+                    res.append(line[1].strip(" "))
+
+                if "pct_correct_notdogs" in line:
+                    line = line.split(": ")
+                    res.append(line[1].strip(" "))
+
+                line = res_file.readline()
+
+        final_res[file] = res
+    # printing the systematized results in a table
+    print()
+    print(10*"-" + "***systematized results***" + 10*"-")
+    print()
+    print(120*"-")
+    print("|cnn model architecture: |{:25} {:25} {:25} {:25}".format("|% not-a-dog-correct|", "|% dogs-correct|", "|% breeds-correct|", "|% match-labels|" ))
+    print(120 * "-")
+    for key in final_res:
+        pct_dogs_correct = final_res[key][0]
+        pct_match_labels = final_res[key][1]
+        pct_breeds_correct = final_res[key][2]
+        pct_not_a_dog_correct = final_res[key][3]
+        cnn = key.split("_")[0]
+        empty = ''
+        print("{:25} {:25} {:25} {:.1f} {:20} {:25}".format(cnn, pct_not_a_dog_correct, pct_dogs_correct, float(pct_breeds_correct),
+                                                            empty, pct_match_labels))
+    print(120 * "-")
+
+
+print_tuple = ("resnet_pet-images.txt", "alexnet_pet-images.txt", "vgg_pet-images.txt")
+sys_res(print_tuple)
